e2d6d58b664063a395da8d56977687e7dd3b5b58
JKson <jkson.min@mootech.asia>
2026-07-16 14:28:04 +0800

    refactor(theme): decouple app entry from concrete skin

--- vue3-app/src/main.js
+++ vue3-app/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue';
 import './assets/design-system/tokens.css';
 import './assets/layout.css';
 import './assets/main.css';
-import './assets/skins/blue.css';
 import './assets/design-system/components.css';
 import './assets/load-more.css';
 
